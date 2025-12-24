55606a7745e4348a22f7f17146672b6b3a4411b5
Manpreet Singh <manpreetkamboj60.net@gmail.com>
2025-12-23 20:31:41 -0500

    Completed Auth API's (Login, Register, Logout, Profile Details)

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { ResponseHandler } from "../utils/response.handler";
 import { verifyAccessToken } from "../utils/token.util";
+import { Role } from "../generated/prisma";
 
 export const isAuthenticated = async (
   req: Request,
@@ -20,6 +21,54 @@ export const isAuthenticated = async (
     if (error instanceof Error) {
       return ResponseHandler.error(res, error.message, 401, null);
     }
+    return ResponseHandler.error(res, "Unauthorized", 401, null);
+  }
+};
+
+export const isPatient = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.user.role !== Role.PATIENT) {
+      return ResponseHandler.error(res, "Unauthorized", 401, null);
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    return ResponseHandler.error(res, "Unauthorized", 401, null);
+  }
+};
+
+export const isStaff = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.user.role !== Role.STAFF) {
+      return ResponseHandler.error(res, "Unauthorized", 401, null);
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    return ResponseHandler.error(res, "Unauthorized", 401, null);
+  }
+};
+
+export const isAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.user.role !== Role.ADMIN) {
+      return ResponseHandler.error(res, "Unauthorized", 401, null);
+    }
+    next();
+  } catch (error) {
+    console.error(error);
     return ResponseHandler.error(res, "Unauthorized", 401, null);
   }
 };
